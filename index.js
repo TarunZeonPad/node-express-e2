ef9334b5c217a6c5fb7b9da8c9974c8f7a22a968
tarun.kumar <tarun.kumar@metricstream.com>
2023-06-22 23:41:51 +0530

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ import express from 'express'
 
 const app = express()
 
+const AmazonDaxClient = require('amazon-dax-client');
+var AWS = require("aws-sdk");
+
+var region = "eu-north-1";
+
+AWS.config.update({
+  region: region
+});
+
+var ddbClient = new AWS.DynamoDB.DocumentClient()
+var client = daxClient != null ? daxClient : ddbClient;
+var tableName = "msiversioningtest";
+
+            var params = {
+                TableName: tableName,
+				Key: {
+                "versionNum": 2,
+                "createdBy": 12345
+            }
+            };
+
+            
+        
+    
+    
+
+
 app.listen(5001, () => console.log('listening to port 5001'))
 
-app.get('/', (req, res) => res.json('test api'))
+app.get('/', (req, res) =>  ddbClient.get(params, function(err, data) {
+                if (err) {
+                    res.json(JSON.stringify(err, null, 2));
+                } else {
+                    res.end(data)
+                }
+            })
+			)
